3acc9323d9431303bf212715e231bf91d6ba0ca1
Andrew Yourkin <andrew@yourkin.io>
2022-04-17 04:19:15 +0200

    Updated run all tests script

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose run --rm website-checker pytest -v
+docker-compose run --rm website-checker pytest -vs
+docker-compose run --rm monitoring-writer python -m pytest -vvs
